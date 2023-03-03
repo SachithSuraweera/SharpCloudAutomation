9b7e871d9e6da641e5fc56c502635b96cf6689b4
AlokaWG <wasundara888@gmail.com>
2023-03-03 11:26:12 +0530

    Add CalculatedStoryLIst class

---
+++ SharpCloudAutomation/Utilities/CalculatedStoryList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpCloudAutomation.Utilities
+{
+    public class CalculatedStoryList
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string StoryUrl { get; set; }
+
+    }
+}
