8aed39cefbbdfdbfa27e24739441818f6f35e71c
Yasasvi Jayasena <yasasvi@bistecglobal.com>
2023-03-08 09:39:28 +0530

    Owner User level TC

---
+++ SharpCloudAutomation/PageObjects/MainToolBarPage.cs
@@ -0,0 +1,139 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpCloudAutomation.PageObjects
+{
+    public class MainToolBarPage
+    {
+        private IWebDriver driver;
+        public MainToolBarPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//div[@id='storyToolbar']"))]
+        private IWebElement mtoolBar;
+        public IWebElement getMtoolBar()
+        {
+            return mtoolBar;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//div[@id='undoDropdown']"))]
+        private IWebElement undoIcon;
+        public IWebElement getUndoIcon()
+        {
+            return undoIcon;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//div[@id='dropdownMenu' and @outsideifnot='dropdownMenu, undoDropdown']//li"))]
+        public IList<IWebElement> undoDropdown;
+        public IList<IWebElement> getUndoDropDown()
+        {
+            return undoDropdown;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//span[@aria-label='Undo menu' and @role='button']"))]
+        public IWebElement undoDropdownIcon;
+        public IWebElement getUndoDropDownIcon()
+        {
+            return undoDropdownIcon;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//div[@tooltip='Story Setup']"))]
+        private IWebElement storyIcon;
+        public IWebElement getStoryIcon()
+        {
+            return storyIcon;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//div[@id='dropdownMenu' and @outsideifnot='dropdownMenu, storySetupDropdown']//li"))]
+        public IList<IWebElement> storyDropdown;
+        public IList<IWebElement> getStoryDropDown()
+        {
+            return storyDropdown;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//div[@tooltip='Data Setup']"))]
+        private IWebElement dataIcon;
+        public IWebElement DataIcon()
+        {
+            return dataIcon;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//div[@id='dropdownMenu' and @outsideifnot='dropdownMenu, dataSetupDropdown']//li"))]
+        private IWebElement dataDropdown;
+        public IWebElement DataDropdown()
+        {
+            return dataDropdown;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//span[text()='View']"))]
+        private IWebElement viewIcon;
+        public IWebElement ViewIcon()
+        {
+            return viewIcon;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//button[text()='Reset View']"))]
+        private IWebElement resetIcon;
+        public IWebElement ResetIcon()
+        {
+            return resetIcon;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//button[text()='Save View']"))]
+        private IWebElement saveIcon;
+        public IWebElement SaveIcon()
+        {
+            return saveIcon;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//div[@id='viewSetupDropdown']//li"))]
+        private IWebElement viewSetupDropdown;
+        public IWebElement ViewSetupDropdown()
+        {
+            return viewSetupDropdown;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//div[@id='relationshipsToggle']"))]
+        private IWebElement relationshipIcon;
+        public IWebElement RelationshipIcon()
+        {
+            return relationshipIcon;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//div[@id='relationshipsToggle']//li"))]
+        private IWebElement relationshipDropDown;
+        public IWebElement RelationshipDropDown()
+        {
+            return relationshipDropDown;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//span[text()='Unlock']"))]
+        private IWebElement unlockIcon;
+        public IWebElement UnlockIcon()
+        {
+            return unlockIcon;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//img[@alt='Search']"))]
+        private IWebElement searchIcon;
+        public IWebElement SearchIcon()
+        {
+            return searchIcon;
+        }
+
+        [FindsBy(How = How.XPath, Using = ("//img[@alt='Fullscreen story button']"))]
+        private IWebElement fullScreenIcon;
+        public IWebElement FullScreenIcon()
+        {
+            return fullScreenIcon;
+        }
+    }
+}
--- SharpCloudAutomation/Tests/MainToolBarSettingAccessTestCase/OwnerUserLevels.cs
+++ SharpCloudAutomation/Tests/MainToolBarSettingAccessTestCase/OwnerUserLevels.cs
@@ -1,4 +1,6 @@
 ﻿using AventStack.ExtentReports;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using SharpCloudAutomation.PageObjects;
 using SharpCloudAutomation.Utilities;
 
@@ -23,6 +25,7 @@ namespace SharpCloudAutomation.Tests.MainToolBarSettingAccessTestCase
                 loginPage.getUserName().SendKeys(sc.Username);
                 loginPage.getPassword().SendKeys(sc.Password);
                 loginPage.getGoBtn().Click();
+                Thread.Sleep(5000);
                 GetDriver().Navigate().GoToUrl(sc.StoryUrl);
                 ExtentTest createNode = CreateNode("Owner Level Main Tool Bar Features");
                 if (mtoolBarPage.getMtoolBar().Displayed)
@@ -32,7 +35,7 @@ namespace SharpCloudAutomation.Tests.MainToolBarSettingAccessTestCase
                 else {
                     createNode.Log(Status.Info, "Maintool Bar Element is not Visible");
                 }
-
+                //Undo Visibility
                 if (mtoolBarPage.getUndoIcon().Displayed)
                 {
                     createNode.Log(Status.Info, "Undo Element is Visible");
@@ -40,6 +43,39 @@ namespace SharpCloudAutomation.Tests.MainToolBarSettingAccessTestCase
                 else
                 {
                     createNode.Log(Status.Info, "Undo Element is not Visible");
+                }
+                mtoolBarPage.getUndoDropDownIcon().Click();
+
+                int elementsCount = mtoolBarPage.getUndoDropDown().Count();
+                String[] uSubMenu = { "undo (CTRL+Z)", "redo (CTRL+Y)", "Restore Story" };
+                for (int i = 1; i <= elementsCount; i++)
+                {
+                    IWebElement subUndo = GetDriver().FindElement(By.XPath("//div[@id='dropdownMenu' and @outsideifnot='dropdownMenu, undoDropdown']//li["+ i +"]"));
+                    String subText = subUndo.Text;
+                    if (uSubMenu[i-1]==subText)
+                    {
+                        createNode.Log(Status.Info, subText+" Element is Visible");
+                    }
+                }
+                //Story visibility
+                if (mtoolBarPage.getStoryIcon().Displayed)
+                {
+                    createNode.Log(Status.Info, "Story Element is Visible");
+                }
+                else
+                {
+                    createNode.Log(Status.Info, "Story Element is not Visible");
+                }
+                int storyelementsCount = mtoolBarPage.getStoryDropDown().Count();
+                String[] storySubMenu = { "Setup", "Manage Presentations", "Manage Forms", "Download Story" };
+                for (int i = 1; i <= storyelementsCount; i++)
+                {
+                    IWebElement subStory = GetDriver().FindElement(By.XPath("//div[@id='dropdownMenu' and @outsideifnot='dropdownMenu, storySetupDropdown']//li[" + i + "]"));
+                    String subText = subStory.Text;
+                    if (storySubMenu[i - 1] == subText)
+                    {
+                        createNode.Log(Status.Info, subText + " Element is Visible");
+                    }
                 }
             }
         }
