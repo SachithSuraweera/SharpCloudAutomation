167b2a69b99b872d8ac65d52c6c57caa2637ac1b
SACHITH SURAWEERA <sachith@bistecglobal.com>
2023-03-08 08:44:24 +0530

    Added CalculationStory test

--- SharpCloudAutomation/Tests/CalculationStoryTestCase/CalculationStoryTest.cs
+++ SharpCloudAutomation/Tests/CalculationStoryTestCase/CalculationStoryTest.cs
@@ -3,6 +3,8 @@ using SharpCloudAutomation.Utilities;
 using SharpCloudAutomation.Tests.LoginTestCase;
 using OpenQA.Selenium;
 using System.Collections;
+using AventStack.ExtentReports;
+using OpenQA.Selenium.Support.UI;
 
 namespace SharpCloudAutomation.Tests.CalculationStoryTestCase
 {
@@ -11,63 +13,63 @@ namespace SharpCloudAutomation.Tests.CalculationStoryTestCase
         [Test]
         public void storyTableViewResultColumnCheck()
         {
+            GetDriver().Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            WebDriverWait wait = new WebDriverWait(GetDriver(), TimeSpan.FromSeconds(15));
+            ArrayList arrayList = new ArrayList();
+
             LoginPage loginPage = new LoginPage(GetDriver());
             CalculatedStoryPage calculatedStoryPage = new CalculatedStoryPage(GetDriver());
             loginPage.validLogin(GetJsonData().ExtractInstanceDataJson("username"), GetJsonData().ExtractInstanceDataJson("password"));
-            ArrayList arrayList = new ArrayList();
 
-            Thread.Sleep(5000);
-            // Json get one story{}
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("div[id='dashboardWelcome'] h1")));
 
             var stories = new JsonReader().GetCalculatedStoryList();
             foreach (var story in stories)
             {
                 GetDriver().Navigate().GoToUrl(story.StoryUrl);
-                Thread.Sleep(5000);
-
+                
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("loader")));
+                
                 IList<IWebElement> viewChooserItems = calculatedStoryPage.getView();
                 viewChooserItems = viewChooserItems.Where(viewChooserItem => !viewChooserItem.Text.ToString().Contains("add_new")).ToList();
-                viewChooserItems = viewChooserItems.Where(viewChooserItem => !viewChooserItem.Text.ToString().Contains("Blank View")).ToList();
+                
                 foreach (IWebElement view in viewChooserItems)
                 {
                     {
                         view.Click();
-                        Thread.Sleep(3000);
-                        IWebElement viewName = calculatedStoryPage.getViewName();
+                        wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("loader")));
+                        Thread.Sleep(1000);
+                        string viewName = calculatedStoryPage.getViewName().Text;
                         IList<IWebElement> tableColmCount = calculatedStoryPage.getTableColmCount();
                         IList<IWebElement> rowsCount = calculatedStoryPage.getRowsCount();
-                        Thread.Sleep(2000);
-                        if(tableColmCount.Count == 5)
+                       
+                        for (int i = 1; i <= rowsCount.Count; i++)
                         {
-                            for (int i = 1; i <= rowsCount.Count; i++)
+                            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//table[@id='table-view']/tbody/tr[" + i + "]/td[" + tableColmCount.Count + "]")));
+                            string nameColm = GetDriver().FindElement(By.XPath("//table[@id='table-view']/tbody/tr[" + i + "]/td[1]")).Text;
+                            string resultColm = GetDriver().FindElement(By.XPath("//table[@id='table-view']/tbody/tr[" + i + "]/td["+ tableColmCount.Count +"]")).Text;
+                            TestContext.Progress.WriteLine(resultColm);
+                            if (resultColm == "FAIL" || resultColm == "Fail")
                             {
-                                string nameColm = GetDriver().FindElement(By.XPath("//table[@id='table-view']/tbody/tr["+ i +"]/td[1]")).Text;
-                                string resultColm = GetDriver().FindElement(By.XPath("//table[@id='table-view']/tbody/tr[" + i + "]/td[5]")).Text;
-                                TestContext.Progress.WriteLine(resultColm);
-                                if(resultColm == "FAIL")
+                                TestContext.Progress.WriteLine(viewName);
+                                if (viewName == "")
                                 {
-                                    arrayList.Add(viewName.Text+ ": " + nameColm);
+                                    arrayList.Add(calculatedStoryPage.getRoadMapName().Text + ": " + nameColm);
                                 }
-                            }
-                        }
-                        else if(tableColmCount.Count == 6)
-                        {
-                            for (int i = 1; i <= rowsCount.Count; i++)
-                            {
-                                string nameColm = GetDriver().FindElement(By.XPath("//table[@id='table-view']/tbody/tr[" + i + "]/td[1]")).Text;
-                                string resultColm = GetDriver().FindElement(By.XPath("//table[@id='table-view']/tbody/tr[" + i + "]/td[6]")).Text;
-                                TestContext.Progress.WriteLine(resultColm);
-                                if (resultColm == "FAIL")
+                                else
                                 {
-                                    arrayList.Add(viewName.Text + ": " + nameColm);
+                                    arrayList.Add(calculatedStoryPage.getRoadMapName().Text +": "+viewName + ": " + nameColm);
                                 }
                             }
                         }
                     }
                 }
             }
-
-            
+            ExtentTest storyNode = CreateNode("Calculation Related failed stories");
+            for (int i = 0; i<arrayList.Count; i++)
+            {
+                storyNode.Log(Status.Info, arrayList[i].ToString());
+            }
         }
     }
 }
